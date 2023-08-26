3d4d02ba0221b5fc66eb31e9f7efde7ec5fe3078
sarthak19360 <sarthak19360@gmail.com>
2023-08-27 00:52:26 +0530

    Added new routes to index.js file in server

--- server/index.js
+++ server/index.js
@@ -70,7 +70,78 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/products', (req, res) => {
+    res.status(200).json({
+        products
+    })
+})
 
+app.get('/products/:id', (req, res) => {
+    const { id } = req.params;
+    const item = products.find((product) => product.id === Number(id));
+    if (!item) {
+        return res.status(400).json({
+            message: `Item with id ${id} not found`,
+        });
+    }
+    res.status(200).json({
+        item
+    });
+});
+
+app.post('/login', async (req, res) => {
+    // extract username and password from body
+    const { username, password } = req.body;
+
+    // Check if both username and password are provided
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and password are required' });
+    }
+    // Find the user by username in the users array
+    const user = users.find(u => u.username === username);
+    // if it does not then return username not registered
+    if (!user) {
+        return res.status(400).json({ message: 'Username not registered' });
+    }
+    // Compare the provided password with the stored hashed password
+    const isMatch = (password === user.password);
+    if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid password' });
+    }
+    res.status(200).json({
+        message: 'Logged in successfully',
+    });
+})
+
+app.post('/signup', async (req, res) => {
+    // extract username and password from body
+    const { username, password } = req.body;
+    // Check if both username and password are provided
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and password are required' });
+    }
+    // Find the user by username in the users array
+    const user = users.find(u => u.username === username);
+    // if it does not then return username not registered
+    if (user) {
+        return res.status(400).json({ message: 'Username already exists' });
+    }
+    const newUser = {
+        id: users.length + 1,
+        username: username,
+        password: password,
+    };
+    users.push(newUser);
+    res.status(201).json({
+        message: 'Username registered successfully'
+    });
+})
+
+app.get('/cart',(req,res) => {
+    res.status(200).json({
+        cartProducts
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
